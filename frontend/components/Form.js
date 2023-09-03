a74b1338c0625a0d0d482b6991c782aecc62068c
giahypham <andygpham@gmail.com>
2023-09-02 23:40:13 -0400

    Completed MVPs

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: "",
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.addTodo(this.state.name)
+    this.setState({
+      ...this.state,
+      name: ""
+    })
+  }
+
+  onChange = e => {
+    const {value}= e.target 
+    this.setState({
+      ...this.setState,
+      name: value 
+    })
+  }
   render() {
+    
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input type="text" value={this.state.name} onChange={this.onChange} />
+        <input type='submit' />
+      </form>
     )
   }
 }
